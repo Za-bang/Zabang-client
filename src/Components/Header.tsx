4dd5cfbedd5603a32bf5f19d1040e55e4fdbde1a
namkyung <lnk7576@gmail.com>
2025-08-22 16:57:07 +0900

    design: 알림 아이콘 주석처리

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,13 +2,14 @@
 
 import styles from "./nav.module.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
+// import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerInner}>
-        <NotificationsNoneRoundedIcon fontSize="inherit" className={styles.headerIcon}/>
+        <div></div>
+        {/* <NotificationsNoneRoundedIcon fontSize="inherit" className={styles.headerIcon}/> */}
         <img src="/logo.png" alt="logo" className={styles.logo} />
         <SearchRoundedIcon fontSize="inherit" className={styles.headerIcon}/>
         </div>
