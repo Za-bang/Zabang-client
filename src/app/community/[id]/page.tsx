25960ccd26deebc376d5dba99a69997d861252bc
namkyung <lnk7576@gmail.com>
2025-08-24 02:26:03 +0900

    chore: 배포 위한 임시 주석처리

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,20 +1,20 @@
-import CommunityPostDetail from "../Components/CommunutyPostDetail";
-import { MOCK_COMMUNITY_POST } from "@/data/demoPosts";
+// import CommunityPostDetail from "../Components/CommunutyPostDetail";
+// import { MOCK_COMMUNITY_POST } from "@/data/demoPosts";
 
-export function generateStaticParams() {
-  return MOCK_COMMUNITY_POST.map((p) => ({ id: p.id }));
-}
+// export function generateStaticParams() {
+//   return MOCK_COMMUNITY_POST.map((p) => ({ id: p.id }));
+// }
 
-export default function CommunityPostPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const post = MOCK_COMMUNITY_POST.find((p) => p.id === params.id);
+// export default function CommunityPostPage({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const post = MOCK_COMMUNITY_POST.find((p) => p.id === params.id);
 
-  if (!post) {
-    return <div>게시글을 찾을 수 없습니다.</div>;
-  }
+//   if (!post) {
+//     return <div>게시글을 찾을 수 없습니다.</div>;
+//   }
 
-  return <CommunityPostDetail post={post} />;
-}
+//   return <CommunityPostDetail post={post} />;
+// }
