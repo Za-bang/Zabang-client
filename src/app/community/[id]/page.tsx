4b8027369379c7e9d0aa29be025b076e6a43f0c7
namkyung <lnk7576@gmail.com>
2025-08-24 02:57:26 +0900

    fix: type error

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,27 +1,22 @@
-// import CommunityPostDetail from "../Components/CommunutyPostDetail";
-// import { MOCK_COMMUNITY_POST } from "@/data/demoPosts";
+import CommunityPostDetail from "../Components/CommunutyPostDetail";
+import { MOCK_COMMUNITY_POST } from "@/data/demoPosts";
 
-// export function generateStaticParams() {
-//   return MOCK_COMMUNITY_POST.map((p) => ({ id: p.id }));
-// }
+export function generateStaticParams() {
+  return MOCK_COMMUNITY_POST.map((p) => ({ id: p.id }));
+}
 
-// export default function CommunityPostPage({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   const post = MOCK_COMMUNITY_POST.find((p) => p.id === params.id);
+interface PageProps {
+  params: Promise<{ id: string }>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}
 
-//   if (!post) {
-//     return <div>게시글을 찾을 수 없습니다.</div>;
-//   }
+export default async function CommunityPostPage({ params }: PageProps) {
+  const { id } = await params;
 
-//   return <CommunityPostDetail post={post} />;
-// }
+  const post = MOCK_COMMUNITY_POST.find((p) => p.id === id);
+  if (!post) {
+    return <div>게시글을 찾을 수 없습니다.</div>;
+  }
 
-export default function generateStaticParams() {
-
-  return (
-    <div>임시페이지</div>
-  )
+  return <CommunityPostDetail post={post} />;
 }
