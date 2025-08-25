ac4ba031fd21781e1ec650e419935723a14fd6ab
namkyung <lnk7576@gmail.com>
2025-08-25 23:24:11 +0900

    commit

--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -34,7 +34,7 @@ export default function WritePost() {
     };
 
     try {
-      const res = await fetch("http://localhost:8000/api/posts", {
+      const res = await fetch("BASE_URL/posts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
